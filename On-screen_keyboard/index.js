ac67c6188dc4c0cf8d0b5bad5e91e0eaed695e6b
GuessMF <segas95@yandex.ru>
2022-08-30 12:57:16 +0300

    master

--- On-screen_keyboard/index.js
+++ On-screen_keyboard/index.js
@@ -1,16 +1,27 @@
 let input = document.getElementById("input");
-let keys = document.getElementById("keybord");
+let keys = document.getElementById("keyboard");
+let caps = false;
 
 keys.addEventListener("click", (event) => {
   if (
     event.target.tagName == "BUTTON" &&
     event.target.innerHTML !== "SPACE" &&
-    event.target.innerHTML !== "CAPS LOCK"
+    event.target.innerHTML !== "CAPS LOCK" &&
+    event.target.innerHTML !== "DELETE" && // не работает ДЕЛИТ
+    caps == false
   ) {
+    console.log(event.target.innerHTML);
     input.value += event.target.innerHTML;
   } else if (event.target.innerHTML == "SPACE") {
     input.value += " ";
-  } else if (event.target.innerHTML == "CAPS LOCK") {
-    input.value.toUpperCase;
+  } else if (event.target.innerHTML == "CAPS LOCK" && caps == false) {
+    caps = true;
+  } else if (event.target.innerHTML == "CAPS LOCK" && caps == true) {
+    caps = false;
+  } else if (event.target.tagName !== "DIV") {
+    input.value += event.target.innerHTML.toUpperCase();
+  } else if (event.target.innerHTML == "DELETE") {
+    input.value -= " ";
+    console.log("back");
   }
 });
