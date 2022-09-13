d7073565530ad4c2beaee9491302fbda5823c59a
GuessMF <segas95@yandex.ru>
2022-09-13 17:01:18 +0300

    master

---
+++ Try/try.js
@@ -0,0 +1,43 @@
+let arr = [1, 2, 3, 4, 5, 6];
+
+for (let elem of arr) {
+  console.log(elem);
+}
+
+// let i = 1;
+// while (i < 5) {
+//   console.log("heloo");
+//   i++;
+// }
+
+// let lang = "eu";
+
+// switch (lang) {
+//   case "ru":
+//     alert("ru");
+//     break;
+//   case "en":
+//     alert("en");
+//     break;
+//   case "eu":
+//     alert("eu");
+//     break;
+
+//   default:
+//     alert("язык не поддерживается");
+//     break;
+// }
+
+// let num = 3;
+
+// switch (num) {
+//   case 1:
+//     alert("весна");
+//     break;
+//   case 2:
+//     alert("осень");
+//     break;
+//   case 3:
+//     alert("Зима");
+//     break;
+// }
